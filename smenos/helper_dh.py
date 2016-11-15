556f8b32ea425efb4d257ef8ea60a1536891ada0
Birgit Schachler <RL-INSTITUT\birgit.schachler@solydxk>
2016-11-15 18:42:09 +0100

    Add district heating storage and heat pump

--- smenos/helper_dh.py
+++ smenos/helper_dh.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import os
+import pandas as pd
+
+import helper_heat_pump as hhp
+from oemof.core.network.entities.components import transformers as transformer
+from oemof.core.network.entities import Bus
 
 
-def calc_dh_supply_temp(temp, **kwargs):
+def calc_supply_temp(temp, **kwargs):
     '''
     Generates an hourly supply temperature profile depending on the ambient
     temperature.
@@ -15,6 +19,12 @@ def calc_dh_supply_temp(temp, **kwargs):
     temperature of T_supply_min.
     For ambient temperatures below T_heat_period the supply temperature
     increases linearly to T_supply_max with decreasing ambient temperature.
+    
+    Parameters
+    -----------
+    temp : pandas Series
+    ---------------------
+    kwargs : T_supply_max, T_supply_min, T_heat_period, T_amb_min
     '''
     T_supply_max = kwargs.get('T_supply_max', 135)  # max supply temperature
     T_supply_min = kwargs.get('T_supply_min', 70)  # min supply temperature
@@ -35,6 +45,176 @@ def calc_dh_supply_temp(temp, **kwargs):
     return T_supply
 
 
+def create_heat_storage_entity(uid, cap, dh_bus_entity, region, **kwargs):
+    '''
+    Function to create a district heating heat storage entity.
+    
+    Parameters
+    -----------
+    uid : string
+        unique id of entity
+    cap : float
+        maximum capacity of heat storage
+    dh_bus_entity : oemof.core bus object
+        district heating bus entity
+    region : oemof.core Region object
+        region entity
+    ----------------------------------------
+    kwargs : cap_min, out_max, in_max, eta_in, eta_out, cap_loss, cap_initial,
+        opex_fix, opex_var, capex
+    '''
+    transformer.Storage(uid=uid,
+                        inputs=[dh_bus_entity],
+                        outputs=[dh_bus_entity],
+                        cap_max=cap,
+                        cap_min=kwargs.get('cap_min', None),
+                        out_max=kwargs.get('out_max', cap/2),
+                        in_max=kwargs.get('in_max', kwargs.get(
+                            'out_max', cap/2)),
+                        eta_in=kwargs.get('eta_in', 1),
+                        eta_out=kwargs.get('eta_out', 1),
+                        cap_loss=kwargs.get('cap_loss', 0),
+                        cap_initial=kwargs.get('cap_initial', None),
+                        opex_fix=kwargs.get('opex_fix', 0),
+                        opex_var=kwargs.get('opex_var', 0),
+                        capex=kwargs.get('capex', 0),
+                        regions=[region])
+    return
+    
+
+def calc_cop(supply_temp, max_supply_temp_hp, type_hp, **kwargs):
+    """
+    Returns the COP of a district heating heat pump.
+
+    Parameters
+    -----------
+    supply_temp : pandas Series
+        supply temperature of district heating system
+    max_supply_temp_hp : float or numpy array or pandas Series
+        maximum supply temperature from heat pump to district heating system
+    type_hp : string
+        heat pump type (air or brine; in case of another heat source the
+        heat_source_temp must be given)
+    -------------------------------------
+    kwargs : cop_max, eta_g, heat_source_temp
+    """
+    # get efficiency and supply temperature of heat source
+    if type_hp == 'air':
+        eta_g = kwargs.get('eta_g', 0.3)  # COP/COP_max
+        #TODO change retrieving of air temperature profile
+        heat_source_temp = kwargs.get('heat_source_temp', None)
+        if heat_source_temp is None:
+            filename = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), 'temp'))
+            heat_source_temp = pd.read_pickle(filename)
+    elif type_hp == 'brine':
+        eta_g = kwargs.get('eta_g', 0.4)  # COP/COP_max
+        heat_source_temp = hhp.calc_brine_supply_temp()
+    else:
+        eta_g = kwargs.get('eta_g', 0.4)  # COP/COP_max
+        heat_source_temp = kwargs.get('heat_source_temp', None)
+    # limit supply temperature to maximum supply temperature of the heat pump
+    supply_temp[supply_temp > max_supply_temp_hp] = max_supply_temp_hp
+    # calculate COP
+    cop = eta_g * ((273.15 + supply_temp) / (supply_temp - heat_source_temp))
+    cop_max = kwargs.get('cop_max', 7)
+    cop[cop > cop_max] = cop_max
+    return cop
+    
+    
+def create_hp_entity(uid, cap, dh_bus_entity, el_bus_entity, region,
+                     max_supply_temp_hp, type_hp, **kwargs):
+    '''
+    Creates a heat pump entity in a district heating system. The heat pump uses
+    some kind of heat source to heat the return flow from the district
+    heating system to max_supply_temp_hp.
+    
+    Parameters
+    ----------
+    uid : string
+        unique id of heat pump entity
+    cap : float
+        capacity of heat pump
+    dh_bus_entity : oemof.core bus object
+        district heating bus entity
+    el_bus_entity : oemof.core bus object
+        electricity bus entity
+    region : oemof.core Region object
+        region entity
+    max_supply_temp_hp : float
+        maximum temperature the heat pump can deliver
+    type_hp : string
+        heat pump type (air or brine; in case of another heat source the
+        heat_source_temp must be given)
+    ------------------------------------
+    kwargs : see definitions calc_supply_temp and calc_cop
+    '''
+    # district heating supply temperature
+    supply_temp = calc_supply_temp(region.temp, **kwargs)
+    # COP
+    cop = calc_cop(supply_temp, max_supply_temp_hp, type_hp, **kwargs)
+    # create entity
+    transformer.Simple(
+            uid=uid,
+            inputs=[el_bus_entity],
+            outputs=[dh_bus_entity],
+            out_max=[cap],
+            eta=[cop],
+            regions=[region])
+    return
+    
+ 
+def create_heat_storage_with_immersion_heater_entities(
+    storage_uid, storage_cap, immersion_heater_uid, immersion_heater_cap,
+    immersion_heater_eta, dh_bus_entity, el_bus_entity, region, **kwargs):
+    '''
+    Creates entities heat storage and immersion heater and connects them
+    to a newly created storage bus that is connected to the district
+    heating bus via two transport transformer. 
+    
+    Parameters
+    ----------
+    storage_uid : string
+        unique id of heat storage entity
+    storage_cap : float
+        capacity of heat storage
+    immersion_heater_uid : string
+        unique id of immersion heater entity
+    immersion_heater_cap : float
+        capacity of immersion heater
+    immersion_heater_eta : float
+        efficiency of immersion heater
+    dh_bus_entity : oemof.core bus object
+        district heating bus entity
+    el_bus_entity : oemof.core bus object
+        electricity bus entity
+    region : oemof.core Region object
+        region entity
+    ------------------------------------
+    kwargs : see definitions calc_supply_temp and calc_cop
+    '''
+    # create storage bus
+    storage_bus = Bus(uid=dh_bus_entity.uid+'_storage_bus',
+        type='dh', regions=[region], excess=False)
+    # create heat storage entity
+    create_heat_storage_entity(storage_uid, storage_cap, storage_bus, region)
+    # create immersion heater entity
+    transformer.Simple(
+        uid=immersion_heater_uid,
+        inputs=[el_bus_entity], outputs=[storage_bus],
+        out_max=[immersion_heater_cap], eta=[immersion_heater_eta])
+    # create transports to and from dh bus to storage bus
+    transformer.Simple(
+        uid=dh_bus_entity.uid + '_transport_1',
+        inputs=[el_bus_entity], outputs=[storage_bus],
+        out_max=[max(immersion_heater_cap, storage_cap)], eta=[1])
+    transformer.Simple(
+        uid=dh_bus_entity.uid + '_transport_2',
+        inputs=[storage_bus], outputs=[el_bus_entity],
+        out_max=[max(immersion_heater_cap, storage_cap)], eta=[1])
+    return
+    
+    
 #def add_constraint_dh_heating_storage(om, temp, **kwargs):
 #    '''
 #    Make constraint for post heating of district heating thermal storage in 
@@ -78,17 +258,3 @@ def calc_dh_supply_temp(temp, **kwargs):
 #                    'Provide supply temp ' + str(hour)
 #    return
 #    
-#    
-#
-#
-#
-#
-#filename = os.path.abspath(os.path.join(os.path.dirname(__file__),
-#                                        'temp'))
-#temp = pd.read_pickle(filename)
-#T_supply = calc_dh_supply_temp(temp)
-#for i in list(range(len(T_supply.index))):
-#    print(i)
-##from matplotlib import pyplot as plt
-##T_supply.plot()
-##plt.show()
