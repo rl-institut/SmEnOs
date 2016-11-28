758808e8f398c2f024b12e336b7f3074a1b8c85a
Elisa Gaudchau <Elisa.Gaudchau@rl-institut.local>
2016-11-28 13:15:52 +0100

    add app GVM

--- smenos/appGVM.py
+++ smenos/appGVM.py
@@ -9,8 +9,7 @@ import os
 from oemof import db
 from oemof.tools import logger
 from oemof.core import energy_system as es
-from oemof.db import tools
-#from oemof.db import feedin_pg
+
 from oemof.solph import predefined_objectives as predefined_objectives
 from oemof.core.network.entities import Bus
 from oemof.core.network.entities.components import sinks as sink
@@ -18,20 +17,20 @@ from oemof.solph.optimization_model import OptimizationModel
 from oemof.core.network.entities.components import transformers as transformer
 
 import helper_gvm as hlsg
-import helper_dh as hldh
 
 # choose scenario
-scenario = 'gvm1'
+scenario_base = 'gvm1'
+scenario_heat = 'gvm1'
 schema = 'oemof_test'
 table = 'gvm_transformer'
 
 # Basic inputs
 warnings.simplefilter(action="ignore", category=RuntimeWarning)
 logger.define_logging()
-year = 2010
-time_index = pd.date_range('1/1/{0}'.format(year), periods=2, freq='H')
+year = 2013
+time_index = pd.date_range('1/1/{0}'.format(year), periods=8760, freq='H')
 time_index_demandlib = pd.date_range(
-    '1/1/{0}'.format(year), periods=8736, freq='H')
+    '1/1/{0}'.format(year), periods=8760, freq='H')
 conn = db.connection()
 
 ########### get data ###########################################
@@ -39,7 +38,7 @@ filename = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                         'gvm/timeseries_gvm.csv'))
 times_gvm = pd.read_csv(filename, delimiter=',')
 
-price_el_ts = times_gvm['price_el']
+price_el_ts = times_gvm['price_eta']
 print(price_el_ts)
 refund_el_ts = price_el_ts * (-1)
 eta_import = 1 / price_el_ts
@@ -66,13 +65,23 @@ filename = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                       'temp'))
 temp = pd.read_pickle(filename)
 
+# Add global buses
+typeofgen_global = ['natural_gas', 'biogas', 'natural_gas_cc', 'lignite',
+                    'oil', 'waste', 'hard_coal', 'sewage_gas']
+# Add biomass bus for Berlin and Brandenburg
+for typ in typeofgen_global:
+    Bus(uid="('bus', 'global', '"+typ+"')",
+        type=typ,
+        excess=False, balanced=False,
+        regions=Regions.regions)
+
 # Add electricity and heat sinks and buses for each region
 for region in Regions.regions:
     # create electricity bus
     el_bus = Bus(uid="('bus', '"+region.name+"', 'elec')",
         type='elec',
         regions=[region],
-        excess=True)
+        excess=False)
     # create buses for import and export
     Bus(uid="('bus', '"+region.name+"', 'import')",
         type='elec',
@@ -94,7 +103,7 @@ for region in Regions.regions:
         in_max=[None],
         out_max=[1000000],
         eta=[price_el_ts],
-        opex_var=-1,
+        opex_var=-1,  # bezieht sich auf output
         regions=[region])
 
     transformer.Simple(
@@ -112,7 +121,9 @@ for region in Regions.regions:
     dh_bus = Bus(uid="('bus', '"+region.name+"', 'dh')",
                  type='dh',
                  regions=[region],
-                excess=True)
+                 excess=True,
+                 shortage=True,
+                 shortage_costs=10000)
 
     # create electricity sink
     demand = sink.Simple(uid=('demand', region.name, 'elec'),
@@ -127,55 +138,14 @@ for region in Regions.regions:
                                  "('bus', '"+region.name+"', 'dh')"],
                          val=dh_demand_ts,
                          regions=[region])
-                         
-    # create dh heat pump
-    cap = 80  # muss noch angepasst werden, sollte Grundlast decken können
-    max_supply_temp_hp = 70  # muss noch angepasst werden, sollte so gewählt werden, dass Grundlast gedeckt wird
-    type_hp = 'air'  # muss noch angepasst werden
-    T_supply_max = 95  # Wert laut Datenanfrage, in DB schreiben?
-    T_supply_min = 65  # Wert laut Datenanfrage, in DB schreiben?
-    T_amb_min = -12  # Wert laut Datenanfrage, in DB schreiben?
-#    hldh.create_hp_entity(('transformer', region.name, 'dh', 'heat_pump'),
-#                          cap, dh_bus, el_bus, region,
-#                          max_supply_temp_hp, type_hp,
-#                          T_supply_max=T_supply_max,
-#                          T_supply_min=T_supply_min, T_amb_min=T_amb_min,
-#                          heat_source_temp=temp)           ### Fehler beim erstelln: region obj has no attr temp
-    # create dh heat storage
-    cap = 80  # muss noch angepasst werden
-    out_max = cap / 4  # muss noch angepasst werden
-    in_max = cap / 4  # muss noch angepasst werden
-    eta_in = 0.98  # Wert aus Wittenberg Simulation
-    eta_out = 0.98  # Wert aus Wittenberg Simulation
-    cap_loss = 0.01  # Wert aus Wittenberg Simulation
-    hldh.create_heat_storage_entity(
-        ('storage', region.name, 'dh'), cap, dh_bus, region,
-         out_max=out_max, in_max=in_max, eta_in=eta_in, eta_out=eta_out,
-         cap_loss=cap_loss)
-    # create dh immersion heater
-    cap = 80  # muss noch angepasst werden   
-    eta = 0.98  # muss noch angepasst werden 
-    transformer.Simple(
-        uid=('transformer', region.name, 'dh', 'immersion_heater'),
-        inputs=[el_bus], outputs=[dh_bus],
-        out_max=[cap], eta=[eta])
 
-# Add global buses
-typeofgen_global = ['natural_gas', 'biogas', 'natural_gas_cc', 'lignite',
-                    'oil', 'waste', 'hard_coal', 'sewage_gas']
-# Add biomass bus for Berlin and Brandenburg
-for typ in typeofgen_global:
-    Bus(uid="('bus', 'global', '"+typ+"')",
-        type=typ,
-        excess=False, balanced=False,
-        regions=Regions.regions)
+    region.temp = temp
 
-# create all powerplants from database table
-for region in Regions.regions:
+    # create all powerplants from database table
     logging.info('Processing region: {0}'.format(region.name))
 
-    hlsg.create_powerplants(Regions, region, conn, scenario, schema, table,
-                            year)
+    hlsg.create_powerplants(Regions, region, conn, scenario_base,
+                            scenario_heat, schema, table, year)
 
 # Remove orphan buses
 buses = [obj for obj in Regions.entities if isinstance(obj, Bus)]
--- smenos/helper_gvm.py
+++ smenos/helper_gvm.py
@@ -10,6 +10,7 @@ from oemof.core.network.entities.components import transformers as transformer
 from oemof.core.network.entities.components import sources as source
 import feedin_nominal
 import helper_SmEnOs as hls
+import helper_dh as hldh
 from shapely.wkt import loads as wkt_loads
 
 
@@ -38,10 +39,15 @@ def get_transformer_db(conn, schema, table, scenario, typ):
     return(read_parameter)
 
 
-def create_powerplants(esystem, region, conn, scenario, schema, table, year):
+def create_powerplants(esystem, region, conn, scenario_base, scenario_heat, 
+                       schema, table, year):
 
+    dh_bus = [obj for obj in region.entities if obj.uid ==
+              "('bus', '"+region.name+"', 'dh')"][0]
+    el_bus = [obj for obj in region.entities if obj.uid ==
+              "('bus', '"+region.name+"', 'elec')"][0]
     # create bhkws ###########################################
-    pp = get_transformer_db(conn, schema, table, scenario, 'bhkw')
+    pp = get_transformer_db(conn, schema, table, scenario_base, 'bhkw')
     print('pp_bhkw')
     print(pp)
     if pp.empty:
@@ -61,13 +67,13 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
                              "('bus', '"+region.name+"', 'dh')"][0]],
                     in_max=[None],
                     out_max=[p_el_bhkw, p_th_bhkw],
-                    eta=[row['eta_el'], row['eta_th']],
-                    opex_var=row['opex']/(row['eta_el']+row['eta_th']),                    
+                    eta=[float(row['eta_el']), float(row['eta_th'])],
+                    opex_var=float(row['opex']/(row['eta_el']+row['eta_th'])),                    
                     #input_costs=row['opex'],
                     regions=[region])
 
     # create electric transformer ##############################
-    pp = get_transformer_db(conn, schema, table, scenario,
+    pp = get_transformer_db(conn, schema, table, scenario_base,
                                 'transformer_el')
     print('pp_el')
     print(pp)
@@ -81,15 +87,15 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
                     inputs=[obj for obj in esystem.entities if obj.uid ==
                             "('bus', 'global', '"+row['fuel']+"')"],
                     outputs=[[obj for obj in region.entities if obj.uid ==
-                             "('bus', '"+region.name+"', 'el')"][0]],
+                             "('bus', '"+region.name+"', 'elec')"][0]],
                     in_max=[None],
                     out_max=[float(row['power'])],
-                    eta=[row['eta_el']],
-                    opex_var=row['opex']/row['eta_el'],
+                    eta=[float(row['eta_el'])],
+                    opex_var=float(row['opex']/row['eta_el']),
                     regions=[region])
 
     # create heat transformer #################################
-    pp = get_transformer_db(conn, schema, table, scenario,
+    pp = get_transformer_db(conn, schema, table, scenario_base,
                             'transformer_th')
     print('pp_th')
     print(pp)
@@ -106,12 +112,12 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
                              "('bus', '"+region.name+"', 'dh')"][0]],
                     in_max=[None],
                     out_max=[float(row['power'])],
-                    eta=[row['eta_th']],
-                    opex_var=row['opex']/row['eta_th'],
+                    eta=[float(row['eta_th'])],
+                    opex_var=float(row['opex']/row['eta_th']),
                     regions=[region])
 
     # create powertoheat transformer #################################
-    pp = get_transformer_db(conn, schema, table, scenario,
+    pp = get_transformer_db(conn, schema, table, scenario_heat,
                             'powertoheat')
     print('pp_pth')
     print(pp)
@@ -119,17 +125,60 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
         print('kein powertoheat')
     else:
         for index, row in pp.iterrows():
-            transformer.Simple(
-                    uid=('transformer', region.name, row['plant'], 't_pth'),
-                    inputs=[obj for obj in esystem.entities if obj.uid ==
-                            "('bus', '"+region.name+"', 'el')"],
-                    outputs=[[obj for obj in region.entities if obj.uid ==
-                             "('bus', '"+region.name+"', 'dh')"][0]],
-                    in_max=[None],
-                    out_max=[float(row['power'])],
-                    eta=[row['eta_th']],
-                    opex_var=row['opex']/row['eta_th'],
-                    regions=[region])
+            if row['power'] > 0:
+                transformer.Simple(
+                        uid=('transformer', region.name, row['plant'], 't_pth'),
+                        inputs=[obj for obj in esystem.entities if obj.uid ==
+                                "('bus', '"+region.name+"', 'elec')"],
+                        outputs=[[obj for obj in region.entities if obj.uid ==
+                                 "('bus', '"+region.name+"', 'dh')"][0]],
+                        in_max=[None],
+                        out_max=[float(row['power'])],
+                        eta=[float(row['eta_th'])],
+                        opex_var=float(row['opex']/row['eta_th']),
+                        regions=[region])
+
+    # create dh heat pump
+    pp = get_transformer_db(conn, schema, table, scenario_heat,
+                            'heatpump')
+    if pp.empty:
+        print('keine heatpump')
+    else:
+        for index, row in pp.iterrows():
+            if row['power'] > 0:
+                cap = float(row['power'])  # sollte Grundlast decken können (outputleistung)
+                max_supply_temp_hp = 70  # sollte so gewählt werden, dass Grundlast gedeckt wird, nicht mehr al 80
+                type_hp = 'brine'  # muss noch angepasst werden
+                T_supply_max = 95  #Wert laut Datenanfrage
+                T_supply_min = 65  #Wert laut Datenanfrage
+                T_amb_min = -12  #Wert laut Datenanfrage
+                hldh.create_hp_entity(('transformer', region.name, 'dh', 'heat_pump'),
+                              cap, dh_bus, el_bus, region,
+                              max_supply_temp_hp, type_hp,
+                              T_supply_max=T_supply_max,
+                              T_supply_min=T_supply_min, T_amb_min=T_amb_min,
+                              heat_source_temp=region.temp)
+
+
+    #create dh heat storage
+    pp = get_transformer_db(conn, schema, table, scenario_heat,
+                            'heat_storage')
+    if pp.empty:
+        print('kein heat_storage')
+    else:
+        for index, row in pp.iterrows():
+            if row['power'] > 0:
+                cap = float(row['power'])
+                out_max = cap / 4
+                in_max = cap / 4
+                eta_in = 0.95  # TODO!
+                eta_out = 0.95
+                cap_loss = 0.01  # Wert aus Wittenberg Simulation
+                hldh.create_heat_storage_entity(
+                    ('storage', region.name, 'dh'), cap, dh_bus, region,
+                     out_max=out_max, in_max=in_max, eta_in=eta_in, eta_out=eta_out,
+                     cap_loss=cap_loss)
+
 
     # create renewable power plants ##########################
     # renewable parameters
@@ -139,7 +188,7 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
     print('wind:')
     print(feedin_df['wind_pwr'])
 
-    pp = get_transformer_db(conn, schema, table, scenario, 'pv')
+    pp = get_transformer_db(conn, schema, table, scenario_base, 'pv')
     if pp.empty:
         print('keine pv')
     else:
@@ -151,7 +200,7 @@ def create_powerplants(esystem, region, conn, scenario, schema, table, year):
                 val=feedin_df['pv_pwr'],
                 out_max=[float(row['power'])])
 
-    pp = get_transformer_db(conn, schema, table, scenario, 'wind')
+    pp = get_transformer_db(conn, schema, table, scenario_base, 'wind')
     if pp.empty:
         print('keine windkraft')
     else:
